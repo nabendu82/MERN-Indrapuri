b560a89002d5086ebf53d9af66015e1e9dd27131
nabendu82 <nabendu.biswas@gmail.com>
2023-06-06 19:59:40 +0530

    JS-2

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,22 +1,22 @@
 //Variables
-var name = "Nabendu";
-let age = 41;
-const isDeveloper = true;
+// var name = "Nabendu";
+// let age = 41;
+// const isDeveloper = true;
 // isDeveloper = false;
 // console.log(name);
 // console.log(age);
 // console.log(isDeveloper);
 
-function varExp(){
-    if(true){
-        var num1 = 10;
-        console.log("Inside if var " + num1);
-    }
-    console.log("Outside if var " + num1);
-}
+// function varExp(){
+//     if(true){
+//         var num1 = 10;
+//         console.log("Inside if var " + num1);
+//     }
+//     console.log("Outside if var " + num1);
+// }
 
-varExp()
-console.log("Outside function var " + num1);
+// varExp()
+// console.log("Outside function var " + num1);
 
 
 // if(true){
@@ -24,3 +24,40 @@ console.log("Outside function var " + num1);
 //     console.log("Inside if let " + num2);
 // }
 // console.log("Outside if let " + num2);
+
+//DataTypes
+const name = 'Nabendu';
+const age = 41;
+// const rating = 5.5;
+// const isDev = true;
+// const x = null;
+// let z;
+
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof rating);
+// console.log(typeof isDev);
+// console.log(typeof x);
+// console.log(typeof z);
+
+// const y = null;
+// if(typeof y === 'object'){
+//     console.log('y is null');
+// }
+
+//String concatenation
+// console.log('My name is ' + name + ' and i am ' + age + ' year\'s old');
+// console.log(`My name is ${name} and i am ${age} year's old`);
+
+//String methods
+const str = 'Hello JavaScript';
+
+console.log(str.length);
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str.substring(0, 7));
+console.log(str.substring(3));
+console.log(str.includes('Java'));
+console.log(str.includes('Cobol'));
+console.log(str.split(' '));
+console.log(str.slice(-4));
